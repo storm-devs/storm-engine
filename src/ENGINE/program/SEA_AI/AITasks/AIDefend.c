8a600118e3e5dcf134d1d59e732fabf9775081e2
Nikolai Prigodich <cooodesloth@gmail.com>
2020-10-25 10:45:56 +0100

    Remove ini files and scripts from engine folder

--- src/ENGINE/program/SEA_AI/AITasks/AIDefend.c
+++
@@ -1,3 +0,0 @@
-void AIDefend_CheckTask(string sGroupID)
-{
-}
--- src/ENGINE/program/Weather/init/Common.c
+++
@@ -1,5 +0,0 @@
-void Whr_InitCommon()
-{
-	WhrCommonParams.Sea.Pena.BigIslandMultiply = 0.3;
-	WhrCommonParams.Sea.Pena.SmallIslandMultiply = 1.0;
-}
