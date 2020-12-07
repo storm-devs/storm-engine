fc16119f0b286e2740b7f44f0a04cdba405088c5
espkk <espkk@protonmail.ch>
2020-12-07 19:45:42 +0300

    initial thunderstorm-ccs version
    # Conflicts:
    #	CMakeLists.txt

---
+++ gen-vs-solution-x64.bat
@@ -0,0 +1,4 @@
+@echo off
+mkdir build_x64
+cd build_x64
+cmake -G "Visual Studio 16 2019" -A x64 ..
--- gen-vs-solution-x86.bat
+++ gen-vs-solution-x86.bat
@@ -1,4 +1,4 @@
 @echo off
-mkdir build
-cd build
+mkdir build_x86
+cd build_x86
 cmake -G "Visual Studio 16 2019" -A Win32 ..
