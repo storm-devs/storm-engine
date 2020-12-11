5318797e9c60989d4e5fb07b798b6246b71e3671
espkk <espkk@protonmail.ch>
2020-12-11 23:26:40 +0100

    Build blot library

--- src/libs/Blot/Blots.h
+++ src/libs/Blot/Blots.h
@@ -11,7 +11,7 @@
 #ifndef _Blots_H_
 #define _Blots_H_
 
-#include "dx8render.h"
+#include "dx9render.h"
 #include "model.h"
 #include "vmodule_api.h"
 
@@ -20,18 +20,18 @@
 #define BLOTS_TIME 120.0f //Время жизни пятна
 #define BLOTS_DIST 150.0f //Дистанция видимости в метрах
 
-class Blots : public ENTITY
+class Blots : public Entity
 {
 
 #pragma pack(push, 1)
 
     struct Blot
     {
-        byte isUsed;     //Используеться ли это
-        byte lastAlpha;  //Последние значение альфы
-        word numTrgs;    //Количество треугольников
-        float liveTime;  //Время жизни
-        long startIndex; //Начальный индекс в массиве
+        uint8_t isUsed;    //Используеться ли это
+        uint8_t lastAlpha; //Последние значение альфы
+        uint16_t numTrgs;  //Количество треугольников
+        float liveTime;    //Время жизни
+        long startIndex;   //Начальный индекс в массиве
         long rnd;
         CVECTOR pos, dir;
     };
@@ -55,9 +55,26 @@ class Blots : public ENTITY
     //Инициализация
     bool Init();
     //Сообщения
-    dword _cdecl ProcessMessage(MESSAGE &message);
+    uint64_t ProcessMessage(MESSAGE &message);
+
+    void ProcessStage(Stage stage, uint32_t delta) override
+    {
+        switch (stage)
+        {
+        // case Stage::execute:
+        //	Execute(delta); break;
+        case Stage::realize:
+            Realize(delta);
+            break;
+            /*case Stage::lost_render:
+                LostRender(delta); break;
+            case Stage::restore_render:
+                RestoreRender(delta); break;*/
+        }
+    }
+
     //Работа
-    void Realize(dword delta_time);
+    void Realize(uint32_t delta_time);
 
     //--------------------------------------------------------------------------------------------
     //Инкапсуляция
@@ -75,8 +92,8 @@ class Blots : public ENTITY
     void LoadBlot(long i);
 
   private:
-    VDX8RENDER *rs;
-    ENTITY_ID model;
+    VDX9RENDER *rs;
+    entid_t model;
     long textureID;
     ATTRIBUTES *blotsInfo;
     ATTRIBUTES *pCharAttributeRoot;
