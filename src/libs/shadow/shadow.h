9709798d9d266f2d381467b41ed3c1829bfcdadc
espkk <espkk@protonmail.ch>
2020-12-13 12:43:43 +0100

    Build shadow library

--- src/libs/shadow/shadow.h
+++ src/libs/shadow/shadow.h
@@ -2,18 +2,17 @@
 #define _SHADOW_H_
 
 #include "collide.h"
-#include "dx8render.h"
-#include "geos.h"
+#include "dx9render.h"
 #include "model.h"
 #include "vmodule_api.h"
 
-class SHADOW : public ENTITY
+class Shadow : public Entity
 {
-    VDX8RENDER *rs;
+    VDX9RENDER *rs;
     COLLIDE *col;
     void FindPlanes(const CMatrix &view, const CMatrix &proj);
     PLANE planes[6];
-    ENTITY_ID entity;
+    entid_t entity;
     void Smooth();
     float shading;
     unsigned long blendValue;
@@ -25,11 +24,28 @@ class SHADOW : public ENTITY
         CVECTOR pos;
         float tu, tv;
     };
-    SHADOW();
-    ~SHADOW();
+    Shadow();
+    ~Shadow();
     bool Init();
-    void Realize(dword Delta_Time);
-    dword _cdecl ProcessMessage(MESSAGE &message);
+    void Realize(uint32_t Delta_Time);
+    uint64_t ProcessMessage(MESSAGE &message);
+    void ProcessStage(Stage stage, uint32_t delta) override
+    {
+        switch (stage)
+        {
+        // case Stage::execute:
+        //	Execute(delta); break;
+        case Stage::realize:
+            Realize(delta);
+            break;
+        case Stage::lost_render:
+            LostRender();
+            break;
+        case Stage::restore_render:
+            RestoreRender();
+            break;
+        }
+    }
     void LostRender();
     void RestoreRender();
 };
