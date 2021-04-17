537a55dd98a798563e6df09e70018a917fe77dec
Artem Vorotnikov <artem@vorotnikov.me>
2021-04-17 08:18:53 +0300

    Simplify SERVICES_LIST

--- src/apps/ENGINE/services_list.cpp
+++ src/apps/ENGINE/services_list.cpp
@@ -6,7 +6,6 @@ SERVICES_LIST::SERVICES_LIST()
 {
     List = nullptr;
     Search_module_code = 0;
-    Objects = 0;
 }
 
 SERVICES_LIST::~SERVICES_LIST()
@@ -16,19 +15,17 @@ SERVICES_LIST::~SERVICES_LIST()
 
 SERVICE *SERVICES_LIST::Add(uint32_t module_code, uint32_t class_code, SERVICE *pointer)
 {
-    auto *const Pointer = AddRef(module_code);
-
-    if (Pointer)
-        return Pointer;
+    auto existing_node = FindNode(module_code);
+    if (existing_node)
+    {
+        return existing_node->pointer;
+    }
 
     auto *node_PTR = (SERVICE_NODE *)new char[sizeof(SERVICE_NODE)];
     memset(node_PTR, 0, sizeof(SERVICE_NODE));
     node_PTR->module_code = module_code;
     node_PTR->class_code = class_code;
     node_PTR->pointer = pointer;
-    node_PTR->reference = 1;
-
-    Objects++;
 
     if (List == nullptr)
     {
@@ -38,45 +35,8 @@ SERVICE *SERVICES_LIST::Add(uint32_t module_code, uint32_t class_code, SERVICE *
     }
 
     node_PTR->linkL = List;
-    List->linkR = node_PTR;
     List = node_PTR;
     return node_PTR->pointer;
-}
-
-SERVICE *SERVICES_LIST::AddRef(uint32_t module_code)
-{
-    auto *const node_PTR = FindNode(module_code);
-    if (node_PTR == nullptr)
-        return nullptr;
-    node_PTR->reference++;
-    return node_PTR->pointer;
-}
-
-SERVICE *SERVICES_LIST::Find(uint32_t module_code)
-{
-    auto *node_PTR = List;
-    while (node_PTR)
-    {
-        if (node_PTR->module_code == module_code)
-            return node_PTR->pointer;
-        node_PTR = node_PTR->linkL;
-    }
-    return nullptr;
-}
-
-SERVICE *SERVICES_LIST::Find(uint32_t module_code, uint32_t class_code)
-{
-    auto *node_PTR = List;
-    while (node_PTR)
-    {
-        if (node_PTR->module_code == module_code)
-        {
-            if (node_PTR->class_code == class_code)
-                return node_PTR->pointer;
-        }
-        node_PTR = node_PTR->linkL;
-    }
-    return nullptr;
 }
 
 SERVICE_NODE *SERVICES_LIST::FindNode(uint32_t module_code)
@@ -103,43 +63,6 @@ SERVICE_NODE *SERVICES_LIST::FindNode(SERVICE *sp)
     return nullptr;
 }
 
-uint32_t SERVICES_LIST::Release(uint32_t module_code, uint32_t class_code)
-{
-    uint32_t ref;
-
-    SERVICE_NODE *node_PTR = List;
-    while (node_PTR)
-    {
-        if (node_PTR->module_code == module_code)
-        {
-            if (node_PTR->class_code == class_code)
-            {
-                if (node_PTR->reference > 0)
-                    node_PTR->reference--;
-                ref = node_PTR->reference;
-                if (node_PTR->reference == 0)
-                {
-                    SERVICE *pointer = node_PTR->pointer;
-                    auto *node_L = node_PTR->linkL;
-                    auto *node_R = node_PTR->linkR;
-                    if (node_L)
-                        node_L->linkR = node_R;
-                    if (node_R)
-                        node_R->linkL = node_L;
-                    if (node_PTR == List)
-                        List = node_L;
-                    delete node_PTR;
-                    Objects--;
-                    return 0;
-                }
-            }
-            return ref;
-        }
-        node_PTR = node_PTR->linkL;
-    }
-    return 0;
-}
-
 void SERVICES_LIST::Release()
 {
     SERVICE_NODE *node_PTR = List;
@@ -151,36 +74,6 @@ void SERVICES_LIST::Release()
     }
 
     List = nullptr;
-}
-
-SERVICE *SERVICES_LIST::GetService()
-{
-    if (List)
-    {
-        Search_module_code = List->module_code;
-        return List->pointer;
-    }
-    return nullptr;
-}
-
-SERVICE *SERVICES_LIST::GetServiceNext()
-{
-    SERVICE_NODE *node_PTR = List;
-    while (node_PTR)
-    {
-        if (node_PTR->module_code == Search_module_code)
-        {
-            auto *node_L = node_PTR->linkL;
-            if (node_L)
-            {
-                Search_module_code = node_L->module_code;
-                return node_L->pointer;
-            }
-            return nullptr;
-        }
-        node_PTR = node_PTR->linkL;
-    }
-    return nullptr;
 }
 
 SERVICE *SERVICES_LIST::GetService(uint32_t &class_code)
@@ -213,20 +106,4 @@ SERVICE *SERVICES_LIST::GetServiceNext(uint32_t &class_code)
         node_PTR = node_PTR->linkL;
     }
     return nullptr;
-}
-
-uint32_t SERVICES_LIST::GetRef(SERVICE *sp)
-{
-    SERVICE_NODE *node_PTR = FindNode(sp);
-    if (node_PTR == nullptr)
-        throw std::runtime_error("no service");
-    return node_PTR->reference;
-}
-
-void SERVICES_LIST::SetRef(SERVICE *sp, uint32_t ref)
-{
-    SERVICE_NODE *node_PTR = FindNode(sp);
-    if (node_PTR == nullptr)
-        throw std::runtime_error("no service");
-    node_PTR->reference = ref;
 }
