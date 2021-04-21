78631237bdc41f11b13d7be6b4d2870743f16173
espkk <espkk@protonmail.ch>
2021-04-21 22:10:50 +0300

    - Add binary crashpad handler - Tweak cmakes
    Signed-off-by: espkk <espkk@protonmail.ch>

--- src/apps/ENGINE/ExecutableLifecycleService.hpp
+++ src/apps/ENGINE/ExecutableLifecycleService.hpp
@@ -11,7 +11,7 @@ namespace storm::except
 
 class ExecutableLifecycleService
 {
-public:
+  public:
     ~ExecutableLifecycleService();
 
     bool initialize(bool enableCrashReports);
@@ -19,11 +19,9 @@ public:
     // all attachments should be specified before calling `initialize()`
     void addAttachment(const std::filesystem::path &path);
 
-
-
   protected:
     bool initialized_{false};
     std::set<std::string> attachments_;
 };
 
-}
+} // namespace storm::except
