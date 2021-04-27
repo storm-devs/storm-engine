4a9e50ed9944f4bf339ec40f355a6ed4ea44bbdf
Hammie <sam@hammie.be>
2021-04-27 20:17:30 +0200

    Comment Caribbean Tales as Storm Engine 2.5

--- src/libs/Common_h/engine_version.hpp
+++ src/libs/Common_h/engine_version.hpp
@@ -6,10 +6,10 @@ namespace storm
 enum class ENGINE_VERSION
 {
     UNKNOWN,
-    SEA_DOGS, // 1.0 - Will likely never be supported
+    SEA_DOGS,                 // 1.0 - Will likely never be supported
     PIRATES_OF_THE_CARIBBEAN, // 2.0
-    CARIBBEAN_TALES,
-    CITY_OF_ABANDONED_SHIPS, // 2.8
+    CARIBBEAN_TALES,          // 2.5
+    CITY_OF_ABANDONED_SHIPS,  // 2.8
     TO_EACH_HIS_OWN,
     LATEST, // Current, modified version
 };
