23427c1a9680280f24ab42c4ba1e2bd68d029f6b
Nikolai Prigodich <cooodesloth@gmail.com>
2021-05-04 23:01:47 +0200

    Fix sorting of empty strings in dump_save

--- tools/gamesave-convert/gamesave_convert.py
+++ tools/gamesave-convert/gamesave_convert.py
@@ -43,7 +43,7 @@ def read_int8_16_32(buffer, cur_ptr):
 def read_string(buffer, cur_ptr, encoding):
     str_len, cur_ptr = read_int8_16_32(buffer, cur_ptr)
     if str_len == 0:
-        return None, cur_ptr
+        return '', cur_ptr
     s = struct.unpack_from(f'{str_len-1}s', buffer, cur_ptr)[0]  # str_len-1 to skip trailing '\0'
     s = s.decode(encoding)
     cur_ptr += str_len
@@ -214,7 +214,7 @@ def write_int8_16_32(value, buffer):
 
 
 def write_string(s, buffer, encoding):
-    if s is not None:
+    if s:
         s = s.encode(encoding) + b'\x00'
         str_len = len(s)
         buffer = write_int8_16_32(str_len, buffer)
