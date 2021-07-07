7450e9cc226e7afb7dfcf6fc4f2405e5f928a0af
Artess999 <artess999@gmail.com>
2021-07-07 19:01:15 +0300

    blender-gm-import: fix opposite

--- tools/blender-gm-import/gm-reader.js
+++ tools/blender-gm-import/gm-reader.js
@@ -724,7 +724,9 @@ export class GmReader {
         },
       }) => [r / 255, g / 255, b / 255, a / 255]);
 
-      const faces = this.triangles.slice(striangle, striangle + ntriangles).map(([v1, v2, v3]) => [v1, v2, v3]);
+      const faces = this.triangles
+        .slice(striangle, striangle + ntriangles)
+        .map(([v1, v2, v3]) => [v2, v1, v3]); // opposite
 
       objects.push({
         name,
