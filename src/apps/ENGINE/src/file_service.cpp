17f697da821519ed050879568777d467d0ccbdb7
espkk <espkk@protonmail.ch>
2021-07-29 08:09:10 +0300

    engine: bring back RefDec() to INIFILE_T should soulve #89

--- src/apps/ENGINE/src/file_service.cpp
+++ src/apps/ENGINE/src/file_service.cpp
@@ -353,6 +353,21 @@ bool FILE_SERVICE::LoadFile(const char *file_name, char **ppBuffer, uint32_t *dw
 
 //=================================================================================================
 
+INIFILE_T::~INIFILE_T()
+{
+    if (auto fileService = dynamic_cast<FILE_SERVICE *>(fio); fileService)
+    {
+        try
+        {
+            fileService->RefDec(ifs_PTR);
+        }
+        catch (const std::exception &e)
+        {
+            spdlog::error(e.what());
+        }
+    }
+}
+
 void INIFILE_T::AddString(const char *section_name, const char *key_name, const char *string)
 {
     ifs_PTR->AddString(section_name, key_name, string);
