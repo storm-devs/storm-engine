aaef17769e88b2ff3b284eb3d51383818a97b6e6
Nikolai Prigodich <cooodesloth@gmail.com>
2021-09-01 22:01:00 +0200

    Add seasave serialization stubs

--- tools/gamesave-convert/seasave.py
+++ tools/gamesave-convert/seasave.py
@@ -781,5 +781,450 @@ def read_seasave(buffer):
 
     return seasave
 
-def write_seasave():
-    return
+
+# Serialization
+
+def write_buffer(buf, buffer):
+    size = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    buf = struct.unpack_from(f'{size}s', buffer, cur_ptr)[0]
+    cur_ptr += size
+    return buffer
+
+
+def write_string(s, buffer, encoding):
+    str_len = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    if str_len == 0:
+        return '', cur_ptr
+    s = struct.unpack_from(f'{str_len - 1}s', buffer, cur_ptr)[0]  # str_len-1 to skip trailing '\0'
+    s = s.decode(encoding)
+    cur_ptr += str_len
+    return buffer
+
+
+def write_attr_ptr(attr_ptr, buffer):
+    index = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    s, cur_ptr = read_string(buffer, cur_ptr, 'cp1251')
+    attr_ptr = {
+        'index': index,
+        'str': s
+    }
+    return buffer
+
+
+def write_aiballs(aiballs, buffer):
+    num_ball_types = 4
+    for _ in range(num_ball_types):
+        size = struct.unpack_from('I', buffer, cur_ptr)[0]
+        cur_ptr += 4
+        for _ in range(size):
+            first_pos = struct.unpack_from('3f', buffer, cur_ptr)[0]
+            cur_ptr += 3 * 4
+            pos = struct.unpack_from('3f', buffer, cur_ptr)[0]
+            cur_ptr += 3 * 4
+            particle_ptr = struct.unpack_from('p', buffer, cur_ptr)[0]
+            cur_ptr += 8
+            ball_event, cur_ptr = read_string(buffer, cur_ptr, 'cp1251')
+
+            format = 'IffffffffffI'
+            data = struct.unpack_from(format, buffer, cur_ptr)
+            cur_ptr += struct.calcsize(format)
+
+            ball_params = {
+                'first_pos': first_pos,
+                'pos': pos,
+                'particle_ptr': particle_ptr,
+                'ball_event': ball_event,
+                'ball_owner': data[0],
+                'time': data[1],
+                'speedV0': data[2],
+                'dir_x': data[3],
+                'dir_z': data[4],
+                'sin_angle': data[5],
+                'cos_angle': data[6],
+                'height_multiply': data[7],
+                'size_multiply': data[8],
+                'time_speed_multiply': data[9],
+                'max_fire_distance': data[10],
+                'cannon_type': data[11]
+            }
+            assert (len(data) == 12)
+
+    return buffer
+
+
+def write_aicannon(aicannon, buffer):
+    format = '3f3ffff3fIfIIIII'
+    data = struct.unpack_from(format, buffer, cur_ptr)
+    cur_ptr += struct.calcsize(format)
+
+    cannon = {
+        'pos': data[0],
+        'dir': data[1],
+        'time2action': data[2],
+        'total_time2action': data[3],
+        'speedV0': data[4],
+        'enemy_pos': data[5],
+        'empty': data[6],
+        'damaged': data[7],
+        'fired': data[8],
+        'ready2fire': data[9],
+        'recharged': data[10],
+        'load': data[11],
+        'can_recharge': data[12]
+    }
+    assert (len(data) == 13)
+    return buffer
+
+
+def write_aifort(aifort, buffer):
+    min_cannon_damage_distance = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    num_forts = 4
+    for _ in range(num_forts):
+        pos = struct.unpack_from('3f', buffer, cur_ptr)[0]
+        cur_ptr += 4
+        num_cannons = 4
+        for _ in range(num_cannons):
+            cannon, cur_ptr = read_aicannon(buffer, cur_ptr)
+        num_culevrins = 4
+        for _ in range(num_culevrins):
+            culevrin, cur_ptr = read_aicannon(buffer, cur_ptr)
+        num_mortars = 4
+        for _ in range(num_cannons):
+            mortar, cur_ptr = read_aicannon(buffer, cur_ptr)
+    return buffer
+
+
+def write_ship(ship, buffer):
+    character, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+    ship, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+    realize_layer, cur_ptr = read_string(buffer, cur_ptr)
+    execute_layer, cur_ptr = read_string(buffer, cur_ptr)
+    ship_name, cur_ptr = read_string(buffer, cur_ptr)
+    _ = struct.unpack_from('I', buffer, cur_ptr)[0]  # skip
+    cur_ptr += 4
+    gravity = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    sail_state = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    uni_idx = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    use = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    ship_points, cur_ptr = read_buffer(buffer, cur_ptr)
+    speed_accel = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    sp, cur_ptr = read_buffer(buffer, cur_ptr)
+    pos = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    angle = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    water_line = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    is_dead = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    is_visible = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    dead_dir = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    cur_dead_dir = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    keel_contour, cur_ptr = read_buffer(buffer, cur_ptr)
+    is_ship2strand = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    is_mounted = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    is_keel_contour = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    is_perk_turn_active = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    initial_perk_angle = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    result_perk_angle = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    strength, cur_ptr = read_buffer(buffer, cur_ptr)
+    is_set_light_and_fog = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    save_ambient = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    save_fog_color = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    save_light, cur_ptr = read_buffer(buffer, cur_ptr)
+    state, cur_ptr = read_buffer(buffer, cur_ptr)
+
+    num_masts = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    masts = []
+    for _ in range(num_masts):
+        format = '3f3fIIf'
+        data = struct.unpack_from(format, buffer, cur_ptr)
+        cur_ptr += struct.calcsize(format)
+
+        mast = {
+            'v_src': {'x': data[0], 'y': data[1], 'z': data[2]},
+            'v_dst': {'x': data[3], 'y': data[4], 'z': data[5]},
+            'mast_num': data[6],
+            'is_broken': data[7],
+            'damage': data[8]
+        }
+        assert (len(data) == 9)
+
+    num_hulls = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    hulls = []
+    for _ in range(num_hulls):
+        format = '3f3fIIf'
+        data = struct.unpack_from(format, buffer, cur_ptr)
+        cur_ptr += struct.calcsize(format)
+
+        hull = {
+            'v_src': {'x': data[0], 'y': data[1], 'z': data[2]},
+            'v_dst': {'x': data[3], 'y': data[4], 'z': data[5]},
+            'hull_num': data[6],
+            'is_broken': data[7],
+            'damage': data[8]
+        }
+        assert (len(data) == 9)
+
+    num_fireplaces = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    fireplaces = []
+    for _ in range(num_fireplaces):
+        fireplace, cur_ptr = read_fireplace(buffer, cur_ptr)
+
+    x_heel = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    z_heel = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+
+    return buffer
+
+
+def write_aiship(aiship, buffer):
+    ship, cur_ptr = read_ship(buffer, cur_ptr)
+
+    character, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+    obj_type = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    is_dead = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+
+    cannon_ctrl, cur_ptr = read_aiship_cannon_controller(buffer, cur_ptr)
+
+    has_cam_ctrl = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    if has_cam_ctrl:
+        cam_ctrl, cur_ptr = read_aiship_camera_controller(buffer, cur_ptr)
+
+    move_ctrl, cur_ptr = read_aiship_move_controller(buffer, cur_ptr)
+    rotate_ctrl, cur_ptr = read_aiship_rotate_controller(buffer, cur_ptr)
+    speed_ctrl, cur_ptr = read_aiship_speed_controller(buffer, cur_ptr)
+    task_ctrl, cur_ptr = read_aiship_task_controller(buffer, cur_ptr)
+    touch_ctrl, cur_ptr = read_aiship_touch_controller(buffer, cur_ptr)
+
+    return buffer
+
+
+def write_aiship_camera_controller(controller, buffer):
+    return buffer
+
+
+def write_aiship_cannon_controller(controller, buffer):
+    return buffer
+
+
+def write_aiship_move_controller(controller, buffer):
+    return buffer
+
+
+def write_aiship_rotate_controller(controller, buffer):
+    return buffer
+
+
+def write_aiship_speed_controller(controller, buffer):
+    return buffer
+
+
+def write_aiship_task_controller(controller, buffer):
+    return buffer
+
+
+def write_aiship_touch_controller(controller, buffer):
+    return buffer
+
+
+def write_aigroup(aigroup, buffer):
+    commander, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+    group_name, cur_ptr = read_string(buffer, cur_ptr)
+    command, cur_ptr = read_string(buffer, cur_ptr)
+    command_group, cur_ptr = read_string(buffer, cur_ptr)
+    location_near_other_group, cur_ptr = read_string(buffer, cur_ptr)
+    group_type, cur_ptr = read_string(buffer, cur_ptr)
+    init_group_pos = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    move_point = struct.unpack_from('3f', buffer, cur_ptr)[0]
+    cur_ptr += 3 * 4
+    first_execute = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    num_ships = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    for _ in range(num_ships):
+        ship, cur_ptr = read_ship(buffer, cur_ptr)
+
+    return buffer
+
+
+def write_aihelper(aihelper, buffer):
+    gravity = struct.unpack_from('f', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    num_relations = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    relations, cur_ptr = read_buffer(buffer, cur_ptr)
+    sea_cameras, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+
+    num_characters = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    characters = []
+    for _ in range(num_characters):
+        char_ptr, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+        characters.append(char_ptr)
+
+    num_main_characters = struct.unpack_from('I', buffer, cur_ptr)[0]
+    cur_ptr += 4
+    main_characters = []
+    for _ in range(num_main_characters):
+        char_ptr, cur_ptr = read_attr_ptr(buffer, cur_ptr)
+        main_characters.append(char_ptr)
+
+    aihelper = {
+        'gravity': gravity,
+        'num_relations': num_relations,
+        'relations': relations,
+        'sea_cameras': sea_cameras,
+        'characters': characters,
+        'main_characters': main_characters
+    }
+    return buffer
+
+
+def write_deck_cam(deck_cam, buffer):
+    vertices, cur_ptr = read_buffer(buffer, cur_ptr)
+
+    format = '13f3f3f3f3f3ff'
+    data = struct.unpack_from(format, buffer, cur_ptr)
+    cur_ptr += struct.calcsize(format)
+
+    deck_cam = {
+        'distance_sensivity': data[0],
+        'height_angle_sensivity': data[1],
+        'azimuth_angle_sensivity': data[2],
+        'rocking_x': data[3],
+        'rocking_z': data[4],
+        'men_step_up': data[5],
+        'men_step_min': data[6],
+        'height_max': data[7],
+        'height_min': data[8],
+        'height_step': data[9],
+        'cam_max_x': data[10],
+        'cam_min_x': data[11],
+        'default_height': data[12],
+        'g_vec1': {'x': data[13], 'y': data[14], 'z': data[15]},
+        'g_vec2': {'x': data[16], 'y': data[17], 'z': data[18]},
+        'g_vec3': {'x': data[19], 'y': data[20], 'z': data[21]},
+        'cam_pos': {'x': data[22], 'y': data[23], 'z': data[24]},
+        'cam_angle': {'x': data[25], 'y': data[26], 'z': data[27]},
+        'eye_height': data[28]
+    }
+    assert (len(data) == 29)
+
+    deck_cam['screen_rect'], cur_ptr = read_buffer(buffer, cur_ptr)
+
+    format = '4If'
+    data = struct.unpack_from(format, buffer, cur_ptr)
+    cur_ptr += struct.calcsize(format)
+    deck_cam['lock_x'] = data[0]
+    deck_cam['lock_y'] = data[1]
+    deck_cam['is_on'] = data[2]
+    deck_cam['is_active'] = data[3]
+    deck_cam['perspective'] = data[4]
+    assert (len(data) == 5)
+
+    deck_cam['character'], cur_ptr = read_attr_ptr(buffer, cur_ptr)
+
+    return buffer
+
+
+def write_free_cam(free_cam, buffer):
+    format = '3f3ffIIIf'
+    data = struct.unpack_from(format, buffer, cur_ptr)
+    cur_ptr += struct.calcsize(format)
+
+    free_cam = {
+        'pos': {'x': data[0], 'y': data[1], 'z': data[2]},
+        'angle': {'x': data[3], 'y': data[4], 'z': data[5]},
+        'fov': data[6],
+        'lock_x': data[7],
+        'lock_y': data[8],
+        'is_onland': data[9],
+        'cam_onland_height': data[10]
+    }
+    assert (len(data) == 11)
+
+    return buffer
+
+
+def write_ship_cam(ship_cam, buffer):
+    buffer += struct.pack('I', ship_cam['lock_x'])
+    buffer += struct.pack('I', ship_cam['lock_y'])
+    buffer += struct.pack('f', ship_cam['min_height_on_sea'])
+    buffer += struct.pack('f', ship_cam['max_height_on_sea'])
+    buffer += struct.pack('f', ship_cam['distance'])
+    buffer += struct.pack('f', ship_cam['max_distance'])
+    buffer += struct.pack('f', ship_cam['min_distance'])
+    buffer += struct.pack('f', ship_cam['distance_delta'])
+    buffer += struct.pack('f', ship_cam['distance_inertia'])
+    buffer += struct.pack('f', ship_cam['min_angle_x'])
+    buffer += struct.pack('f', ship_cam['max_angle_x'])
+    buffer += struct.pack('f', ship_cam['angle_x_delta'])
+    buffer += struct.pack('f', ship_cam['angle_x_inertia'])
+    buffer += struct.pack('f', ship_cam['angle_y_delta'])
+    buffer += struct.pack('f', ship_cam['angle_y_inertia'])
+    buffer += struct.pack('f', ship_cam['distance_sensivity'])
+    buffer += struct.pack('f', ship_cam['azimuth_angle_sensivity'])
+    buffer += struct.pack('f', ship_cam['height_angle_sensivity'])
+    buffer += struct.pack('f', ship_cam['invert_mouse_x'])
+    buffer += struct.pack('f', ship_cam['invert_mouse_y'])
+    buffer += struct.pack('3f', ship_cam['center']['x'], ship_cam['center']['y'], ship_cam['center']['z'])
+    buffer += struct.pack('3f', ship_cam['angle']['x'], ship_cam['angle']['y'], ship_cam['angle']['z'])
+    buffer += struct.pack('f', ship_cam['model_atan_y'])
+    buffer += struct.pack('I', ship_cam['ship_code'])
+    buffer += struct.pack('I', ship_cam['num_islands'])
+    buffer += struct.pack('I', ship_cam['is_on'])
+    buffer += struct.pack('I', ship_cam['is_active'])
+    buffer += struct.pack('f', ship_cam['perspective'])
+
+    ship_cam['character'], cur_ptr = read_attr_ptr(buffer, cur_ptr)
+
+    return buffer
+
+
+def write_fireplace(fireplace, buffer):
+    buffer += struct.pack('I', var['type'].value)
+    buffer += struct.pack('I', len(var['values']))
+
+    return buffer
+
+
+def write_seasave(seasave, buffer):
+    buffer = write_aihelper(aihelper, buffer)
+
+    buffer = write_ship_cam(ship_cam, buffer)
+    buffer = write_free_cam(free_cam, buffer)
+    buffer = write_deck_cam(deck_cam, buffer)
+
+    buffer = write_aiballs(aiballs, buffer)
+
+    return buffer
