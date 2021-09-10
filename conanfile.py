166208672dd88e8f41309ee1158e497b5bd6b2bc
espkk <espkk@protonmail.ch>
2021-09-10 03:48:50 +0300

    meta: move resource deploy to conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,12 +1,15 @@
 from conans import ConanFile
+from os import getenv
+from distutils.dir_util import copy_tree
 
 class StormEngine(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
 
     # build options provided by CMakeLists.txt that are used in conanfile.py
     options = {
+        "output_directory": "ANY",
         "crash_reports": ["ON", "OFF"],
-        "steam": ["ON", "OFF"],
+        "steam": ["ON", "OFF"]
     }
 
     # dependencies used in deploy binaries
@@ -18,8 +21,8 @@ class StormEngine(ConanFile):
     "directx/9.0@storm/prebuilt", "fmod/2.1.11@storm/prebuilt"]
     # aux dependencies (e.g. for tests)
     build_requires = "catch2/2.13.7"
-    # optional dependencies
 
+    # optional dependencies
     def requirements(self):
         if self.options.steam == "ON":
             self.requires("steamworks/1.5.1@storm/prebuilt")
@@ -33,12 +36,20 @@ class StormEngine(ConanFile):
     }
 
     def __install_bin(self, name):
-        self.copy(name, dst="bin/"+str(self.settings.build_type), src="bin")
+        self.copy(name, dst=self.__dest, src="bin")
 
     def __intall_lib(self, name):
-        self.copy(name, dst="bin/"+str(self.settings.build_type), src="lib")
+        self.copy(name, dst=self.__dest, src="lib")
+
+    def __install_folder(self, src, dst):
+        copy_tree(self.recipe_folder + src, self.__dest + dst)
 
     def imports(self):
+        self.__dest = str(self.options.output_directory) + "/" + getenv("CONAN_IMPORT_PATH", "bin")
+
+        self.__install_folder("/src/techniques", "/resource/techniques")
+        self.__install_folder("/src/libs/shared_headers/include/shared", "/resource/shared")
+
         if self.settings.build_type == "Debug":
             self.__intall_lib("fmodL.dll")
         else:
