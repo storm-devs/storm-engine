97ca6d4d25e393ac7aef4a4f8912a5d637a0d110
espkk <espkk@protonmail.ch>
2021-09-21 14:16:40 +0300

    renderer: fix UpdateSurface() when msaa enabled

--- src/libs/renderer/src/sdevice.cpp
+++ src/libs/renderer/src/sdevice.cpp
@@ -3772,7 +3772,10 @@ HRESULT DX9RENDER::GetSurfaceLevel(IDirect3DTexture9 *ppTexture, UINT Level, IDi
 HRESULT DX9RENDER::UpdateSurface(IDirect3DSurface9 *pSourceSurface, CONST RECT *pSourceRectsArray, UINT cRects,
                                  IDirect3DSurface9 *pDestinationSurface, CONST POINT *pDestPointsArray)
 {
-    return CHECKD3DERR(d3d9->UpdateSurface(pSourceSurface, pSourceRectsArray, pDestinationSurface, pDestPointsArray));
+    return CHECKD3DERR(D3DXLoadSurfaceFromSurface(pDestinationSurface, nullptr, nullptr, pSourceSurface, nullptr,
+                                                 nullptr,
+                                      D3DX_DEFAULT, 0));
+    //return CHECKD3DERR(d3d9->UpdateSurface(pSourceSurface, pSourceRectsArray, pDestinationSurface, pDestPointsArray));
 }
 
 HRESULT DX9RENDER::StretchRect(IDirect3DSurface9 *pSourceSurface, const RECT *pSourceRect,
