48a7be47418465d18b4742c2b0a201bdaf444537
Nikolai Prigodich <cooodesloth@gmail.com>
2021-10-18 21:08:31 +0200

    Don't fail bulk conversion on errors

--- tools/gamesave-convert/convert_all.py
+++ tools/gamesave-convert/convert_all.py
@@ -7,4 +7,7 @@ if __name__ == '__main__':
     files = glob.iglob('./SAVE/**/*', recursive=True)
     for filename in files:
         if os.path.isfile(filename):
-            gamesave_convert.process_file(filename)
+            try:
+                gamesave_convert.process_file(filename)
+            except:
+                print(f'Error processing file {filename}')
