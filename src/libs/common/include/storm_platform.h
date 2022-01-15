9c1b6343a4e1ed488ddfd7a877555113658e9da7
Aleksey Komarov <q4arus@ya.ru>
2022-01-15 12:37:22 +0300

    [linux] use custom strcpy_s instead of safeclib

--- src/libs/common/include/storm_platform.h
+++ src/libs/common/include/storm_platform.h
@@ -1,7 +1,43 @@
 #ifndef _WIN32
 
 #include "safe_str_lib.h"
-// use custom sprintf_s instead of safeclib
+
+// use custom strcpy_s instead of safeclib: #define strcpy_s(dest, dmax, src)
+#undef strcpy_s
+
+inline int strcpy_s(char *dest, size_t size, const char *src)
+{
+    if (!dest)
+        return EINVAL;
+
+    if (0 == size)
+    {
+        dest[0] = '\0';
+        return ERANGE;
+    }
+
+    if (!src)
+    {
+        dest[0] = '\0';
+        return EINVAL;
+    }
+
+    size_t i;
+    for (i = 0; i < size; i++)
+    {
+        if ((dest[i] = src[i]) == '\0')
+            return 0;
+    }
+    dest[0] = '\0';
+    return ERANGE;
+}
+
+template <std::size_t size> inline int strcpy_s(char (&dest)[size], const char *src)
+{
+    return strcpy_s(dest, size, src);
+}
+
+// use custom sprintf_s instead of safeclib: #define sprintf_s(dest, dmax, ...)
 #undef sprintf_s
 
 template <size_t size> inline int sprintf_s(char (&buffer)[size], const char *format, ...)
